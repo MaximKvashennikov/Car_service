70ba74f3f53740fe14dd482e4c48ea29b8e2fefc
MaximKvashennikov <kvashennickov.maxim@yandex.ru>
2022-09-16 11:21:28 +0300

    Add: connect the template index.html

--- car_service/main_app/views.py
+++ car_service/main_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def homepage(request):
-    return HttpResponse("Test main_app")
+    return render(request, "index.html")
