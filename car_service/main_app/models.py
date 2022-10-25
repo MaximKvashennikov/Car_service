f71061f9c7baf54c136391287737316eaa5d2c86
MaximKvashennikov <kvashennickov.maxim@yandex.ru>
2022-10-25 19:03:45 +0300

    Add: contacts panel, model "owner"

--- car_service/main_app/models.py
+++ car_service/main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Owner(models.Model):
+    contact_name = models.CharField(verbose_name="ФИО", max_length=200, blank=True)
+    phone_number = PhoneNumberField(verbose_name="Телефон")
+
+    class Meta:
+        verbose_name = "Номер телефона"
+        verbose_name_plural = "Номера телефонов"
+
+    def __str__(self):
+        return f"{self.phone_number}"
--- car_service/main_app/views.py
+++ car_service/main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Owner
 
 
 def homepage(request):
-    return render(request, "index.html")
+    owner = Owner.objects.all()
+    return render(request, "index.html", {'owner': owner})
