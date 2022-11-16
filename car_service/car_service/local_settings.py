97f8381beb9a290ecf7cc9293834825f4960b55c
MaximKvashennikov <kvashennickov.maxim@yandex.ru>
2022-11-16 13:41:36 +0300

    Add: gunicorn config

--- car_service/car_service/local_settings.py
+++ car_service/car_service/local_settings.py
@@ -2,7 +2,7 @@ SECRET_KEY = "django-insecure-d!-j-a)ttcp*itk(10y@7%ieeua17uei(_6ye!1n$o4mqg3@v%
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 DATABASES = {
     'default': {
